17bf9c461165023dc05eee5e97fca9f1590fbaf9
v3lixsa <134130186+v3lixsa@users.noreply.github.com>
2023-05-23 20:14:15 +0400

    Add files via upload
    İnstagram : ficane.0nlyy
    Don't forget to follow up. 💝

---
+++ termux-setup.py
@@ -0,0 +1,51 @@
+import os
+
+print("""
+88888888888                              .d8888b.           888
+    888                                 d88P  Y88b          888
+    888                                 Y88b.               888
+    888   .d88b.  888d888 88888b.d88b.   "Y888b.    .d88b.  888888 888  888 88888b.
+    888  d8P  Y8b 888P"   888 "888 "88b     "Y88b. d8P  Y8b 888    888  888 888 "88b
+    888  88888888 888     888  888  888       "888 88888888 888    888  888 888  888
+    888  Y8b.     888     888  888  888 Y88b  d88P Y8b.     Y88b.  Y88b 888 888 d88P
+    888   "Y8888  888     888  888  888  "Y8888P"   "Y8888   "Y888  "Y88888 88888P"
+                                                                            888
+                                                                            888
+                                                                            888
+
+Başlamak için (1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""")
+
+secim = input("Kurulum başlasın mı: ")
+
+if(secim == "1"):
+
+    os.system("pkg install git")
+    os.system("pkg install wget")
+    os.system("pkg install python2")
+    os.system("pkg install php")
+    os.system("pkg install perl")
+    os.system("pkg install nano")
+    os.system("pkg install vim")
+    os.system("pkg install cat")
+    os.system("pkg install pip")
+    os.system("pip install wordlist")
+    os.system("pkg install nmap")
+    os.system("pkg install hydra")
+    os.system("pkg install openssl")
+    os.system("apt install nodejs")
